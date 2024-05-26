cb295a925fcf529bed279cb42c94f26a49a4e97a
Luigi Lasso <llasso05@gmail.com>
2024-05-25 21:23:30 -0500

    addint spacing functions and fake list loop

--- elementary_ink.py
+++ elementary_ink.py
@@ -24,3 +24,32 @@ subtitle.alignment = 1
 doc.add_heading('', 1)
 doc.add_paragraph('December 17, 2015')
 doc.add_paragraph('')
+
+
+def set_spacing(paragraph):
+    # Use docx to set line spacing between pargraphs
+    paragraph_format = paragraph.paragraph_format
+    paragraph_format.space_before = Pt(0)
+    paragraph_format.space_after = Pt(0)
+
+legnth_real = len(real_list)
+count_real = 0 # index of current line in real (hidden) message
+ 
+# interleave real anmd fake message l8ines
+for line in fake_list:
+    if count_real < legnth_real and line == "":
+        paragraph = doc.add_paragraph(real_list[count_real])
+        paragraph_index = len(doc.paragraphs) - 1
+        # set real message color to white
+        run = doc.paragraphs[paragraph_index]
+        font = run.font
+        font.color.rgb = RGBColor(255,255,255) # make it red to test
+        count_real += 1
+    else:
+        paragraph = doc.add_paragraph(line)
+    
+    set_spacing(paragraph)
+
+doc.save('ciphertext_message_letterhead.docx')
+
+print("Done")
